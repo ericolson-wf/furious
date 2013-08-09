7e370f0412921da3dacdd2c145c3f4d5d5b275bb
Eric Olson <eric.olson@webfilings.com>
2013-08-09 16:49:26 -0500

    Add unittests for future_tasks_get_result()
    Also make sure the contents of the return list are correctly
    formatted as FutureInfo namedtuples.

--- furious/context/task_futures.py
+++ furious/context/task_futures.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2012 WebFilings, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from collections import namedtuple
 
@@ -42,14 +57,24 @@ def future_tasks_get_result(futures):
 
             count = len(tasks)
             if count <= 1:
+                # Adding this single task failed, so don't try adding it again.
                 return
 
             queue = future_info.queue
             transactional = future_info.transactional
 
+            # Reinsert half
+            half_tasks_1 = tasks[:count / 2]
+            new_future_1 = insert_tasks_async(
+                half_tasks_1, queue, transactional)
             reinserted_futures.append(
-                _insert_tasks_async(tasks[:count / 2], queue, transactional))
+                FutureInfo(new_future_1, half_tasks_1, queue, transactional))
+
+            # Reinsert other half
+            half_tasks_2 = tasks[count / 2:]
+            new_future_2 = insert_tasks_async(
+                half_tasks_2, queue, transactional)
             reinserted_futures.append(
-                _insert_tasks_async(tasks[count / 2:], queue, transactional))
+                FutureInfo(new_future_2, half_tasks_2, queue, transactional))
 
     return reinserted_futures
--- furious/tests/context/test_task_futures.py
+++ furious/tests/context/test_task_futures.py
@@ -20,20 +20,25 @@ from mock import patch
 from mock import Mock
 
 from furious.context.task_futures import insert_tasks_async
+from furious.context.task_futures import future_tasks_get_result
+from furious.context.task_futures import FutureInfo
 
 
 class TestInsertTasksAsync(unittest.TestCase):
-    """Test _insert_tasks_async()."""
+    """Test insert_tasks_async()."""
 
     @patch('google.appengine.api.taskqueue.Queue.add_async', auto_spec=True)
     def test_no_tasks(self, add_async):
+        """When no tasks are passed in, ensure insert_tasks_async() does not
+        raise exceptions and return None.
+        """
 
         tasks = []
         queue, transactional = Mock(), Mock()
         return_value = insert_tasks_async(
             tasks, queue, transactional=transactional)
 
-        # Since we had not tasks, ensure add_async() was not called.
+        # Since we had no tasks, ensure add_async() was not called.
         add_async.assert_calleds_once_with(tasks, transactional=transactional)
 
         # Ensure None was returned
@@ -41,6 +46,9 @@ class TestInsertTasksAsync(unittest.TestCase):
 
     @patch('google.appengine.api.taskqueue.Queue.add_async', auto_spec=True)
     def test_with_tasks(self, add_async):
+        """When tasks are passed to, insert_tasks_async() does not raise
+        exceptions and return None.
+        """
 
         tasks = ["task1", "task2"]
         queue = "queue-name"
@@ -70,3 +78,80 @@ class TestInsertTasksAsync(unittest.TestCase):
         # Ensure the resulting future was returned in a FutureInfo struct.
         future = add_async.return_value
         self.assertEqual(future_info, (future, tasks, queue, transactional))
+
+
+class TestFutureTasksGetResult(unittest.TestCase):
+    """Test future_tasks_get_result()."""
+
+    @patch('furious.context.task_futures.insert_tasks_async', auto_spec=True)
+    def test_no_futures(self, insert_tasks_async):
+        """When we don't pass any futures, just make sure no exception is
+        raised and an empty list is returned.
+        """
+
+        futures = []
+
+        reinserted_futures = future_tasks_get_result(futures)
+
+        # Since we had no futures, ensure insert_tasks_async() was not called.
+        self.assertEqual(0, insert_tasks_async.call_count)
+
+        # Ensure None was returned
+        self.assertEqual([], reinserted_futures)
+
+    @patch('furious.context.task_futures.insert_tasks_async', auto_spec=True)
+    def test_with_futures(self, insert_tasks_async):
+        """When we pass in futures to future_tasks_get_result(), ensure that
+        get_result() was called on each future.
+        Also, since the future's get_result() doesn't raise exceptions, ensure
+        an empty list is returned.
+        """
+
+        transactional = False
+        future_info1 = FutureInfo(
+            Mock(), ["task1"], "queuename", transactional)
+        future_info2 = FutureInfo(
+            Mock(), ["task2", "task3"], "queuename", transactional)
+        futures = [future_info1, future_info2]
+
+        reinserted_futures = future_tasks_get_result(futures)
+
+        # Ensure get_result() was called on the futures.
+        future_info1[0].get_result.assert_called_once_with()
+        future_info2[0].get_result.assert_called_once_with()
+
+        # Since we had no futures, ensure insert_tasks_async() was not called.
+        self.assertEqual(0, insert_tasks_async.call_count)
+
+        # Ensure None was returned
+        self.assertEqual([], reinserted_futures)
+
+    @patch('furious.context.task_futures.insert_tasks_async', auto_spec=True)
+    def test_with_future_raising_exception(self, insert_tasks_async):
+        """When future.get_result raises an Exception, ensure that
+        insert_tasks_async() is called, once for each half of the tasks.
+        """
+
+        from google.appengine.api import taskqueue
+
+        transactional = False
+        future = Mock()
+        future.get_result.side_effect = taskqueue.BadTaskStateError
+
+        future_info1 = FutureInfo(
+            future, ["task1", "task2", "task3", "task4"], "queuename",
+            transactional)
+        futures = [future_info1]
+
+        # Setup the return values for insert_tasks_async()
+        insert_tasks_async.side_effect = ['new_future1', 'new_future2']
+
+        reinserted_futures = future_tasks_get_result(futures)
+
+        expected_reinsert1 = FutureInfo(
+            'new_future1', ['task1', 'task2'], "queuename", transactional)
+        self.assertEqual(reinserted_futures[0], expected_reinsert1)
+
+        expected_reinsert2 = FutureInfo(
+            'new_future2', ['task3', 'task4'], "queuename", transactional)
+        self.assertEqual(reinserted_futures[1], expected_reinsert2)
